9d4671fcf6c4d4b42180d0f7a89cb8b7460d9b84
karchx <kenethra.74@gmail.com>
2025-08-03 22:09:42 -0600

    feat: init haskell project

---
+++ app/Main.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import qualified MyLib (someFunc)
+
+import Control.Monad (foldM)
+import Data.List.NonEmpty
+import Data.Text     (Text)
+import Text.Printf   (printf)
+
+import qualified BlackHoleProvider
+import           SubFSM         ( )
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Haskell!"
+  MyLib.someFunc

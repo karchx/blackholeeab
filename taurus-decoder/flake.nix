5514b23b0f404e7dabdb08357caa973dc40d2317
karchx <kenethra.74@gmail.com>
2025-09-28 14:58:07 -0600

    feat: connection kafka and extract pvD

--- taurus-decoder/flake.nix
+++ taurus-decoder/flake.nix
@@ -15,10 +15,17 @@
           haskell.compiler.ghc96
           haskellPackages.cabal-install
           gmp
+          rdkafka
         ];
 
         # Navigate to the project directory when entering the shell
         shellHook = ''
+           PATH=~/.cabal/bin:$PATH
+           LD_LIBRARY_PATH=${pkgs.zlib}/lib:$LD_LIBRARY_PATH
+           export LIBRARY_PATH=${pkgs.rdkafka}/lib
+           export C_INCLUDE_PATH=${pkgs.rdkafka}/include
+
+           # export KAFKA_TEST_BROKER=$(ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p' | head -n 1)
           cd taurus-decoder
           echo "Haskell devShell"
         '';
--- taurus-decoder/src/Main.hs
+++ taurus-decoder/src/Main.hs
@@ -1,24 +1,117 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DeriveGeneric #-}
+
 module Main (main) where
 
-import qualified Data.Fits as FITS
-import Data.Fits ( HeaderDataUnit(..), dimensions, bitpix, axes )
+import Data.Fits ( HeaderDataUnit(..), dimensions, bitpix, axes, Axes, isBitPixFloat, mainData, pixsUnwrapD, pixDimsByCol, parsePix )
 import Data.Fits.Read ( readHDUs )
+import qualified Data.Vector as V
 import qualified Data.ByteString as BS
-
+import qualified Data.ByteString.Lazy as LBS
+import Statistics.Sample ( range, mean, variance, stdDev, stdErrMean )
+import Kafka.Producer
+import Control.Exception (bracket)
+import Control.Monad (forM_)
+import Data.Aeson (encode, ToJSON, FromJSON)
+import Data.Array.IArray (Array, array)
+import GHC.Generics (Generic)
 import Lens.Micro ((^.))
+
+data RawData = RawData
+    { rdHeader :: String
+    , rdPixels :: V.Vector Double
+    } deriving (Show, Generic)
+instance ToJSON RawData
+instance FromJSON RawData
+
+-- Kafka configuration
+producerProps :: ProducerProperties
+producerProps = brokersList ["localhost:9092"]
+                <> sendTimeout (Timeout 10000)
+                <> setCallback (deliveryCallback print)
+                <> logLevel KafkaLogDebug
+
+targetTopic :: TopicName
+targetTopic = "blackholes-raw"
+
+mkMessage :: Maybe BS.ByteString -> Maybe BS.ByteString -> ProducerRecord
+mkMessage k v  = ProducerRecord
+    { prTopic = targetTopic
+    , prPartition = UnassignedPartition
+    , prKey = k
+    , prValue = v
+    , prHeaders = mempty
+    }
+
+runProducerRaw :: RawData -> IO ()
+runProducerRaw rawJson =
+    bracket mkProducer clProducer (runHandler rawJson) >>= print
+    where
+        mkProducer = newProducer producerProps
+        clProducer (Left _) = return ()
+        clProducer (Right prod) = closeProducer prod
+        runHandler _ (Left err) = return $ Left err
+        runHandler val (Right prod) = sendMessages prod val
+
+sendMessages :: KafkaProducer -> RawData -> IO (Either KafkaError ())
+sendMessages prod rawJson = do
+    putStrLn "[DEBUG] Producer is ready"
+    let payload = LBS.toStrict $ encode rawJson
+    err1 <- produceMessage prod (mkMessage (Just "zero") (Just payload))
+    forM_ err1 print
+
+    return $ Right ()
 
 loadFits :: FilePath -> IO BS.ByteString
 loadFits path = BS.readFile path
 
 processHDU :: HeaderDataUnit -> IO ()
 processHDU hdu = do
-    let header = hdu ^. FITS.header
-        dims = hdu ^. dimensions
-        bpf = dims ^. bitpix
+        putStrLn $ "[DEBUG] Format bits: " ++ show bpf ++ "\n"
+        let pixCount = sum (pixDimsByCol $ (hdu ^. dimensions . axes))
+        pixs <- parsePix pixCount bpf (LBS.fromStrict pd)
+        let pxsD = if isBitPixFloat bpf then pixsUnwrapD bpf pixs else []
+            pVD  = V.fromList pxsD
+        let img = toImageArray pVD
+        putStrLn $ "[DEBUG] Image array shape: " ++ show img
+        if (length ax == 2) && isBitPixFloat bpf
+            then do
+                let raw = bitMapProcess ax pVD
+                runProducerRaw raw
+                print raw
+            else
+                putStrLn $ "[DEBUG] Skipping non 2D float bitmap HDU.\n"
+    where
+        pd = hdu ^. mainData
         ax = hdu ^. dimensions . axes
-    putStrLn $ "Format bits: " ++ show bpf ++ "\n"
-    putStrLn $ "Axes: " ++ show ax ++ "\n"
-    mapM_ (\a -> putStrLn $ "Axis: " ++ show a ++ "\n") ax
+        bpf = hdu ^. dimensions . bitpix
+
+toImageArray :: V.Vector Double -> Array (Int, Int, Int) Double
+toImageArray v = array ((0,0,0), (199,199,3)) $ do
+   i <- [0..199]
+   j <- [0..199]
+   k <- [0..2]
+
+   pure ((i,j,k), v V.! (k*200*200 + j*200 + i))
+
+
+bitMapProcess :: Axes          -- ^ Metadata about the column oriented axes
+               -> V.Vector Double -- ^ Data is stored in column-row major order
+               -> RawData
+bitMapProcess []    _   = RawData ("[ERROR] BitMap processing run with no axes.\n") V.empty
+bitMapProcess [_]   _   = RawData ("[ERROR] BitMap processing run with only one axis.\n") V.empty
+bitMapProcess (_:_) v =
+    RawData
+        { rdHeader = "DATARAW"
+        , rdPixels = V.fromList
+            [ mean v
+            , range v
+            , variance v
+            , stdDev v
+            , stdErrMean v
+            ]
+        }
 
 main :: IO ()
 main = do
